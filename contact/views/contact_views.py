ced8e8173e6081fcbb98a4408364403d689b070f
Marcos Pimentel <marcos.pimenthel@gmail.com>
2024-02-28 22:33:45 -0300

    Estudos de partial

---
+++ contact/views/contact_views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404, render
+from contact.models import Contact
+
+
+def index(request):
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    site_title =f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )

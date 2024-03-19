ac1428b1ab76fa1be2bb023122d9b000b2450d38
Marcos Pimentel <marcos.pimenthel@gmail.com>
2024-03-19 11:42:06 -0300

    dia 19-03

--- contact/forms.py
+++ contact/forms.py
@@ -3,40 +3,44 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
-            }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={'accept' : 'image/*',}
+        )
+    )  
 
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone', )
+        fields = ('first_name', 'last_name', 'phone',
+                  'email', 'description', 'category',
+                  'picture',
+                   )
 
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name', 
-            ValidationError(
-                'Mensagem de erro', 
-                code= 'invalid'
-                )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
                 code='invalid'
             )
-        )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
         return super().clean()
-                       
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name                       
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -58,7 +58,7 @@ def search(request):
 
 
 def contact(request, contact_id):
-    # single_contact = Contact.objects.filter(pk=contact_id).first()
+   
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )

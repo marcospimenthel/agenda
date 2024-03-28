1988cc32a67a41211ec6d8cfc6c8f6e3b20057de
Marcos Pimentel <marcos.pimenthel@gmail.com>
2024-03-28 09:48:58 -0300

    teste 28.03

--- contact/urls.py
+++ contact/urls.py
@@ -15,4 +15,11 @@ urlpatterns = [
     path('contact/<int:contact_id>/update', views.update, name='update'),
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+  
+
+    # User
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]   
